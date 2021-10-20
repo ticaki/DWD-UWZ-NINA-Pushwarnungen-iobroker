f6fc9d6fc794a1c19b20810cd167ed82ba5a2fb3
ticaki <59184294+ticaki@users.noreply.github.com>
2021-10-20 10:27:45 +0200

    Bugfix 2 UWZ

--- dwd_uwz_nina_script.js
+++ dwd_uwz_nina_script.js
@@ -1,4 +1,4 @@
-//Version 0.97.24.3
+//Version 0.97.24.4
 // Erläuterung Update:
 // Suche im Script nach 123456 und kopiere/ersetze ab diesem Punkt. So braucht ihr die Konfiguration nicht zu erneuern.
 // Das gilt solange die Version nicht im nächsten Abschnitt genannt wird, dann muß man auch die Konfiguration neumachen oder im Forum nach den Änderungen schauen.
@@ -1682,7 +1682,7 @@ function addDatabaseData(id, value, mode, old) {
         let hash = null;
         if (i > -1) hash = warnDatabase.new[i].hash;
         change = removeDatabaseDataID(id);
-        if (value.payload.levelName !== undefined) {
+        if (value.payload !== undefined) {
             warn = getDatabaseData(value, mode);
             if (warn) {
                 warn.areaID = getRegionNameUWZ(id);
